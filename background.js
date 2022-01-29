529f54951a9c4521d815d597e66fbf0524552b8d
rinsuki <428rinsuki+git@gmail.com>
2022-01-30 01:12:11 +0900

    first commit

---
+++ background.js
@@ -0,0 +1,44 @@
+browser.webRequest.onBeforeRequest.addListener(details => {
+    const decoder = new TextDecoder("UTF-8")
+    const encoder = new TextEncoder()
+    const filter = browser.webRequest.filterResponseData(details.requestId)
+    let str = ""
+    filter.ondata = event => {
+        str += decoder.decode(event.data, { stream: true })
+    }
+    filter.onstop = () => {
+        console.log(str)
+        const parser = new DOMParser()
+        const dom = parser.parseFromString(str, "application/xml")
+        // MarlinContentIds が入っている時はなんか特殊なDRMでL1だと最高画質が引けないことがあるっぽいので遠慮しておく
+        if (!str.includes("MarlinContentIds")) {
+            for (const adaptionSet of dom.querySelectorAll(`AdaptationSet[contentType=video]`)) {
+                const reprs = adaptionSet.querySelectorAll("Representation")
+                let mostGoodRepr
+                for (const repr of reprs) {
+                    const r = {
+                        dom: repr,
+                        bandwidth: parseInt(repr.getAttribute("bandwidth"), 10),
+                        resolution: parseInt(repr.getAttribute("width"), 10) * parseInt(repr.getAttribute("height"), 10),
+                    }
+                    if (mostGoodRepr == null) {
+                        mostGoodRepr = r
+                    } else if (mostGoodRepr.resolution < r.resolution) {
+                        mostGoodRepr = r
+                    } else if (mostGoodRepr.resolution > r.resolution) {
+                        continue
+                    } else if (mostGoodRepr.bandwidth < r.bandwidth) {
+                        mostGoodRepr = r
+                    }
+                }
+                for (const repr of reprs) {
+                    if (mostGoodRepr.dom !== repr) repr.remove()
+                }
+            }
+        }
+        filter.write(encoder.encode(new XMLSerializer().serializeToString(dom)))
+        filter.disconnect()
+    }
+}, {
+    urls: ["https://*.aiv-cdn.net/*.mpd*"],
+}, ["blocking"])
---
+++ make-zip.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+zip zip/$(date +"%Y%m%d_%H%M%S").zip *.{js,json}
